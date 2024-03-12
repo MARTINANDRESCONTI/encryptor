c56efe051e492c01204cd2bb81bf8db86b8c954c
Martin Conti <matin.a.conti2014@gmail.com>
2024-03-12 01:38:12 -0300

    bugs and refact

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+var foo = document.getElementById('text-output')
+var logoOutput = document.getElementById('logo-output')
+var textOutputContainer = document.getElementById('text-output')
+var paragraphtext = document.createElement("p")
+var cleanInput = () => {document.getElementById('text-input').value = "";}  
+
 function encrypt(){
-  let text = document.getElementById('text-input').value 
-  foo = document.getElementById('text-output')
-   
-  
+  let text = document.getElementById('text-input').value
+
   if(!text){
     return alert("Writte your text")
     }else {
@@ -22,38 +26,34 @@ function encrypt(){
       return e.replace('u', 'ufat');
       }else {
       return e}
-
       }).join('') 
 
-   logoOutput = document.getElementById('logo-output')
-   logoOutput.style.display = 'none'  
+   logoOutput.style.display = 'none'
 
-   textOutputContainer = document.getElementById('text-output')
    textOutputContainer.style.display = 'flex'
 
-   paragraphtext = document.createElement("p")
    paragraphtext.innerText = text
    paragraphtext.setAttribute('class', 'p')
    textOutputContainer.appendChild(paragraphtext) 
    
    }  
-   document.getElementById('text-input').value = "";
+  cleanInput()
 };
 
 function clean (){  
-  textOut = document.getElementById('text-output')
-  textOut.removeChild(textOut.firstElementChild)    
+  textOutputContainer.removeChild(textOutputContainer.firstElementChild)    
 }
 
 function arrowPaste (){
-  textOut = document.querySelectorAll("p")[0].innerText  
-  document.getElementById('text-input').value = textOut  
-  clean()
+  if (textOutputContainer.hasChildNodes()){
+    textOut = document.querySelectorAll("p")[0].innerText  
+    document.getElementById('text-input').value = textOut
+    clean()
+  }
 }
 
 function unencrypt(){
   let text = document.getElementById('text-input').value
-  foo = document.getElementById('text-output')
 
   if(!text){
       return alert("Writte your text")
@@ -63,50 +63,43 @@ function unencrypt(){
       }    
       let textArray = text.split('')
       for(let i = 0; i < textArray.length; i++){
-        if(textArray[i] == 'e'){
+        if(textArray[i] === 'e'){
           textArray.splice(i+1, 4)
-        }else if(textArray[i] == 'i'){
-          textArray.splice(i+1, 3)
-        }else if(textArray[i] == 'a'){
+        }else if(textArray[i] === 'a'){
           textArray.splice(i+1, 1)
-        }else if(textArray[i] == 'o'){
-          textArray.splice(i+1, 3)
-        }else if(textArray[i] == 'u'){
-          textArray.splice(i+1, 3)
-        }
+        }else if(textArray[i] === 'i' || textArray[i] === 'o' || textArray[i] === 'u'){
+          textArray.splice(i+1, 3)}
       }
         text = textArray.join('')
-
-   logoOutput = document.getElementById('logo-output')
+  
    logoOutput.style.display = 'none'  
 
-   textOutputContainer = document.getElementById('text-output')
    textOutputContainer.style.display = 'flex'
 
-   paragraphtext = document.createElement("p")
    paragraphtext.innerText = text
    textOutputContainer.appendChild(paragraphtext)  
    }
-   document.getElementById('text-input').value = "";
+  cleanInput()
 };
 
-const copy = async () => {
-   textOut = document.querySelectorAll("p")[0].innerText
-    try {
-      await navigator.clipboard.writeText(textOut);
-      console.log('Contenido copiado al portapapeles');
-    } catch (err) {
-      console.error('Error al copiar: ', err);
-    }
+async function copy () {
+  if (textOutputContainer.hasChildNodes()){
+    textOut = document.querySelectorAll("p")[0].innerText
+     try {
+       await navigator.clipboard.writeText(textOut);
+       console.log('Contenido copiado al portapapeles');
+     } catch (err) {
+       console.error('Error al copiar: ', err);
+     } 
   }
+}
 
 function del(){
-  textOut = document.getElementById('text-output')
-  textOut.removeChild(textOut.firstElementChild)
-  textOutputContainer = document.getElementById('text-output')
-  textOutputContainer.style.display = 'none'
-  logoOutput = document.getElementById('logo-output')
-  logoOutput.style.display = 'flex'  
+  if (textOutputContainer.hasChildNodes()){
+    textOutputContainer.removeChild(textOutputContainer.firstElementChild)
+    textOutputContainer.style.display = 'none'
+    logoOutput.style.display = 'flex'  
+  }  
 }
 
 
